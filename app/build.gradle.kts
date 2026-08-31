1df1214b9e0180ff53486f3666a704606b1a7b8a
aliumarrr98-create <aliumarrr98@gmail.com>
2026-08-31 07:11:49 +0700

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.datapenjualan"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.datapenjualan"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+}
